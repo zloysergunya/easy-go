5bce8595b190c695092106f1c30f6054830e6a70
zloysergunya <zloysergunya@mail.ru>
2021-09-22 09:54:54 +0700

    Initial Commit

---
+++ SFUHackathon2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFUHackathon2021
+//
+//  Created by Sergey Kotov on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
